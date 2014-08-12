cd73da2ed23d383cafcf4a1e6e2d9260ccccc451
Scott Slater <slaterama@gmail.com>
2014-08-11 23:28:45 -0400

    First simple pattern test successful

--- app/src/main/java/com/slaterama/quantumsheep/view/PatternTwoFragment.java
+++ app/src/main/java/com/slaterama/quantumsheep/view/PatternTwoFragment.java
@@ -34,7 +34,7 @@ public class PatternTwoFragment extends Fragment
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_pattern_one, container, false);
+		return inflater.inflate(R.layout.fragment_pattern_two, container, false);
 	}
 
 	@Override
