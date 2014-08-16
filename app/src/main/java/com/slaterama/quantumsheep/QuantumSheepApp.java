87e22d92fe2dd469a54084941ff51c23cc999b0d
Scott Slater <slaterama@gmail.com>
2014-08-16 09:24:49 -0400

    Removed app label from manifest

--- app/src/main/java/com/slaterama/quantumsheep/QuantumSheepApp.java
+++ app/src/main/java/com/slaterama/quantumsheep/QuantumSheepApp.java
@@ -3,6 +3,7 @@ package com.slaterama.quantumsheep;
 import android.app.Application;
 
 import com.slaterama.qslib.alpha.app.pattern.Pattern;
+import com.slaterama.qslib.utils.AppUtils;
 import com.slaterama.qslib.utils.LogEx;
 import com.slaterama.quantumsheep.pattern_old.TestAppPattern;
 
@@ -13,7 +14,7 @@ public class QuantumSheepApp extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		LogEx.setTagFormat("QuantumSheepTests");
+		LogEx.setTagFormat("%s|%s", AppUtils.getApplicationLabel(this), LogEx.Placeholder.SIMPLE_CLASS_NAME);
 		mAppPattern = new TestAppPattern();
 	}
 }
