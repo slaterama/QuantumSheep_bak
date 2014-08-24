07ac1f41e0a3d969d067969d4f4ee74b6829bb05
Scott Slater <slaterama@gmail.com>
2014-08-24 13:38:30 -0400

    Added TODO section to QuantumSheepApp

--- app/src/main/java/com/slaterama/quantumsheep/QuantumSheepApp.java
+++ app/src/main/java/com/slaterama/quantumsheep/QuantumSheepApp.java
@@ -5,6 +5,12 @@ import android.app.Application;
 import com.slaterama.qslib.utils.AppUtils;
 import com.slaterama.qslib.utils.LogEx;
 
+/* TODO
+	Next steps:
+	1. Figure out how to deal with the redundant "registerPresenter" in Fragments
+	2. Build an "AcceptingEditText" class somehow, with "acceptText()" type functionality
+ */
+
 public class QuantumSheepApp extends Application {
 
 	@Override
